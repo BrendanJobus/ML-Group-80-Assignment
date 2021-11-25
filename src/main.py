4267ea2188af4b768b17bff14638ca9813ef18b9
Brendan Jobus <brendanjobus@gmail.com>
2021-11-25 15:06:29 +0000

    added some base files and some data

---
+++ src/main.py
@@ -0,0 +1,12 @@
+import os.path
+import requests
+
+from pathlib import Path
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
+# TODO: figure out what we're going to use as features
