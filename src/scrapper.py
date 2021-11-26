a9f3e3cc1c5f6c755a89435536b9b1f030a41273
Brendan Jobus <brendanjobus@gmail.com>
2021-11-26 17:30:23 +0000

    merged new_scrapper into scrapper

--- src/scrapper.py
+++ src/scrapper.py
@@ -4,68 +4,85 @@ import re
 import requests 
 import time
 
-def scrape():
-    header = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
-    'referer': 'https://www.zillow.com/brooklyn-new-york-ny/?searchQueryState=%7B%22pagination'
-    }
+header = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+  'referer': 'https://www.zillow.com/brooklyn-new-york-ny/?searchQueryState=%7B%22pagination'
+}
 
-    prices = []
-    beds = []
-    baths = []
-    addresses = []
-    areas = []
+prices = []
+beds = []
+baths = []
+addresses = []
+areas = []
 
-    listOfNeighborhoods = ['Manhattan', 'Brooklyn', 'Bronx', 'Staten-Island', 'Queens']
+listOfNeighborhoods = ['Manhattan', 'Brooklyn', 'Bronx', 'Staten-Island', 'Queens']
 
-    startTime = time.perf_counter()
-    timeSinceLastHibernate = 0
+startTime = time.perf_counter()
+timeSinceLastHibernate = 0
 
-    for neighborhood in listOfNeighborhoods:
-        for page in range (1,2) :
-            if time.perf_counter() - startTime >= 20:
-                print("sleep")
-                time.sleep(20)
-                startTime = time.perf_counter()
-                timeSinceLastHibernate += 20
-            elif timeSinceLastHibernate == 180:
-                time.sleep(120)
-                timeSinceLastHibernate = 0
-            url = f'https://www.zillow.com/{neighborhood}-new-york-ny/{page}_p/'
-            html = requests.get(url=url, headers=header)
-            print(html.content)
-            if html.status_code != 200:
-                break
+for neighborhood in listOfNeighborhoods:
+	for page in range (1,2) :
+		if time.perf_counter() - startTime >= 20:
+			print("sleep")
+			time.sleep(20)
+			startTime = time.perf_counter()
+			timeSinceLastHibernate += 20
+		elif timeSinceLastHibernate == 180:
+			time.sleep(120)
+			timeSinceLastHibernate = 0
 
-            bsobj = soup(html.content, 'html.parser')
-            for element in bsobj.findAll('div', {'class': 'list-card-info'}):
-                if element.find('address', {'class': 'list-card-addr'}):
-                    address = element.find('address', {'class': 'list-card-addr'}).get_text()
-                    price = element.find('div', {'class': 'list-card-price'}).get_text().replace('$','')
-                    element.find('div', {'class': 'list-card-price'}).get_text().replace('$', '')
-                    otherInfo = element.find('ul')
-                    areaInfo = otherInfo.find('abbr', string=re.compile(r'\s*sqft'))
-                    bedInfo = otherInfo.find('abbr', string=re.compile(r'\s*bd.?'))
-                    bathInfo = otherInfo.find('abbr', string=re.compile(r'\s*ba'))
+		url = f'https://www.zillow.com/{neighborhood}-new-york-ny/{page}_p/'
+		html = requests.get(url=url, headers=header)
+		if html.status_code != 200:
+			break
 
-                    print(areaInfo)
+		bsobj = soup(html.content, 'html.parser')
+		for element in bsobj.findAll('div', {'class': 'list-card-info'}):
+			for a in element.find_all('a', href=True):
+				print("Found the URL:", a['href'])
+				getSummary = True
+				getDetails = True
+				while(getSummary or getDetails):
+					href_html = requests.get(url=a['href'], headers=header)
+					if href_html.status_code != 200:
+						break
 
-                    bed = 0 if bedInfo == None else bedInfo.parent.get_text().split(' ')[0]
-                    bath = 0 if bathInfo == None else bathInfo.parent.get_text().split(' ')[0]
-                    area = 0 if areaInfo == None else areaInfo.parent.get_text().split(' ')[0]
-                    prices.append(price)
-                    beds.append(bed)
-                    baths.append(bath)
-                    addresses.append(address)
-                    areas.append(area)
-                    print(address, price, bed, bath, area)
-                    #print(address, price)         
+					href_bsobj = soup(href_html.content, 'html.parser')
+					if href_bsobj.find('div', {'class': 'summary-container'}) and getSummary:
+						data = href_bsobj.find('div', {'class': 'summary-container'}).get_text()
+						print(data)
+						getSummary = False
+					if href_bsobj.find('ul', {'class': 'hdp__sc-1m6tl3b-0 gpsjXQ'}) and getDetails:
+						details = href_bsobj.find('ul', {'class': 'hdp__sc-1m6tl3b-0 gpsjXQ'}).get_text()
+						print(details)
+						getDetails = False
+					else:
+						time.sleep(3)
 
-    df = pd.DataFrame({'Address': addresses, 'Price': prices, 'Beds': beds, 'Baths': baths, 'Area': areas})
-    #df = pd.DataFrame({'Address': addresses, 'Price': prices})
-    df.to_csv('data/listings.csv', index=False, encoding='utf-8')
-    print(df.size)
+					# print(details)
+				# else:
+				# 	print(href_bsobj.prettify())
 
-def exists():
-    print("hi")
 
-scrape()
+#         for element in bsobj.findAll('div', {'class': 'list-card-info'}):
+#             if element.find('address', {'class': 'list-card-addr'}):
+#                 address = element.find('address', {'class': 'list-card-addr'}).get_text()
+#                 price = element.find('div', {'class': 'list-card-price'}).get_text().replace('$','')
+#                 otherInfo = element.findAll('li')
+#                 otherer = element.find('ul')
+#                 print(otherInfo)
+#                 #bed = otherInfo[0].get_text().split(' ')[0]
+#                 #bath = otherInfo[1].get_text().split(' ')[0]
+#                 #area = otherInfo[2].get_text().split(' ')[0]
+#                 prices.append(price)
+#                 #beds.append(bed)
+#                 #baths.append(bath)
+#                 addresses.append(address)
+#                 #areas.append(area)
+#                 #print(address, price, bed, bath, area)
+#                 print(address, price)
+
+# #df = pd.DataFrame({'Address': addresses, 'Price': prices, 'Beds': beds, 'Baths': baths, 'Area': areas})
+# df = pd.DataFrame({'Address': addresses, 'Price': prices})
+# df.to_csv('listings.csv', index=False, encoding='utf-8')
+
+# print(df.size)
