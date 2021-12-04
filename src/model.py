147416d5c74a86bdb40ff80c79407d05b6b0fbf5
Hugo-99 <46873317+Hugo-99@users.noreply.github.com>
2021-12-04 22:11:06 +0000

    Implement model_polynomials for plots
    Remove 'k' from each kfold functions where 5 is the default value, implement poly_cluster to apply polyfeatures with cluster

--- src/model.py
+++ src/model.py
@@ -17,8 +17,9 @@ def minMax(x):
 
 def normalize(df):
     listOfMinMax = df.apply(minMax)
-    index = ['Beds','Baths','Area','Price']
+    index = ['Beds','Baths','Area','Price'] 
     norm = df.copy()
+    print(listOfMinMax)
     for i in index:
         min = listOfMinMax[i][0]
         max = listOfMinMax[i][1]
@@ -26,8 +27,8 @@ def normalize(df):
         norm[i] = (norm[i] - min) / gap
     return norm
 
-def kfold_calculation(X, y, k, model):
-    kf = KFold(n_splits=k)
+def kfold_calculation(X, y, model):
+    kf = KFold(n_splits=5)
     scores=[]
     for train, test in kf.split(X):
         model.fit(X[train], y[train])
@@ -35,100 +36,133 @@ def kfold_calculation(X, y, k, model):
         scores.append(mean_squared_error(y[test],ypred))
     return scores
 
-def kfold_polynomials(X, y, k):
+def poly_cluster(X, q):
+    cluster = pd.get_dummies(X['cluster_label'], prefix='cluster')
+    X = X.drop(['cluster_label'],axis=1)
+    Xpoly = PolynomialFeatures(q).fit_transform(X)
+    Xpoly = np.column_stack((Xpoly,cluster.to_numpy()))
+    return Xpoly
+
+def kfold_polynomials(X, y, model, q_range):
     mean_error=[]; std_error=[]
-    q_range = range(1,7)
+    for q in q_range:
+        Xpoly = poly_cluster(X, q)
+        scores = kfold_calculation(Xpoly, y, model)
+        mean_error.append(np.array(scores).mean())
+        std_error.append(np.array(scores).std())
+    return mean_error, std_error
+
+def model_polynomials(X, y):
+    q_range = range(1,4)
+    models = [LinearRegression(), XGBRegressor(), RandomForestRegressor(), DummyRegressor(strategy='mean')]
+    res = []
+    for m in models:
+        m_err, s_err = kfold_polynomials(X, y, m, q_range)
+        res.append([m_err, s_err])
+    colors = ['r', 'b', 'g', 'y']
+    labels = ['Linear', 'XGB', 'RF', 'Dummy']
+    plt.title('Polynomial features degree choice')
+    for i in range(len(res)):
+        plt.errorbar(q_range,res[i][0],yerr=res[i][1],linewidth=3,color=colors[i])
+        plt.legend(labels[i], numpoints=1)
+        plt.xlabel('q')
+        plt.ylabel('Mean square error')
+        plt.show()
+
+def kfold_Linear(X, y):
+    mean_error=[]; std_error=[]
+    q_range = range(1,3)
     for q in q_range:
         Xpoly = PolynomialFeatures(q).fit_transform(X)
         model = LinearRegression()
-        scores = kfold_calculation(Xpoly, y, k, model)
+        scores = kfold_calculation(Xpoly, y, model)
         mean_error.append(np.array(scores).mean())
         std_error.append(np.array(scores).std())
     plt.title('Polynomial features degree choice')
     plt.errorbar(q_range,mean_error,yerr=std_error,linewidth=3)
     plt.xlabel('q')
     plt.ylabel('Mean square error')
-    #plt.show()
+    plt.show()
 
-def kfold_Lasso(X, y, k, poly):
+def kfold_Lasso(X, y, poly):
     kf = KFold(n_splits=5)
     mean_error=[]; std_error=[]
     Xpoly = PolynomialFeatures(poly).fit_transform(X)
     c_range = [0.0001, 0.001, 0.01, 1, 5, 10, 20, 50]
     for c in c_range:
         model = Lasso(alpha=c)
-        scores = kfold_calculation(Xpoly, y, k, model)
+        scores = kfold_calculation(Xpoly, y, model)
         mean_error.append(np.array(scores).mean())
         std_error.append(np.array(scores).std())
     plt.title('Lasso regression alpha choice')
     plt.errorbar(c_range,mean_error,yerr=std_error,linewidth=3)
     plt.xlabel('c')
     plt.ylabel('Mean square error')
-    #plt.show()
+    plt.show()
 
-def kfold_Ridge(X, y, k, poly):
+def kfold_Ridge(X, y, poly):
     kf = KFold(n_splits=5)
     mean_error=[]; std_error=[]
     Xpoly = PolynomialFeatures(poly).fit_transform(X)
     c_range = [0.0001, 0.001, 0.01, 1, 5, 10, 20, 50]
     for c in c_range:
         model = Ridge(alpha=1/(2*c))
-        scores = kfold_calculation(Xpoly, y, k, model)
+        scores = kfold_calculation(Xpoly, y, model)
         mean_error.append(np.array(scores).mean())
         std_error.append(np.array(scores).std())
     plt.title('Ridge regression alpha choice')
     plt.errorbar(c_range,mean_error,yerr=std_error,linewidth=3)
     plt.xlabel('c')
     plt.ylabel('Mean square error')
-    #plt.show()
+    plt.show()
 
-def kfold_XGBRegressor(X, y, k, poly):
+def kfold_XGBRegressor(X, y, poly):
     kf = KFold(n_splits=5)
     mean_error=[]; std_error=[]
     Xpoly = PolynomialFeatures(poly).fit_transform(X)
     c_range = [0.0001, 0.001, 0.01, 1, 5, 10, 20, 50]
     for c in c_range:
         model = XGBRegressor(alpha=c)
-        scores = kfold_calculation(Xpoly, y, k, model)
+        scores = kfold_calculation(Xpoly, y, model)
         mean_error.append(np.array(scores).mean())
         std_error.append(np.array(scores).std())
     plt.title('XGB regression alpha choice')
     plt.errorbar(c_range,mean_error,yerr=std_error,linewidth=3)
     plt.xlabel('c')
     plt.ylabel('Mean square error')
-    #plt.show()
+    plt.show()
 
-def kfold_RandomForestRegressor(X, y, k, poly):
+def kfold_RandomForestRegressor(X, y, poly):
     kf = KFold(n_splits=5)
     mean_error=[]; std_error=[]
     Xpoly = PolynomialFeatures(poly).fit_transform(X)
     n_range = [1, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
     for n in n_range:
         model = RandomForestRegressor(n_estimators=n)
-        scores = kfold_calculation(Xpoly, y, k, model)
+        scores = kfold_calculation(Xpoly, y, model)
         mean_error.append(np.array(scores).mean())
         std_error.append(np.array(scores).std())
     plt.title('Random Forest Regressor regression n_estimators choice')
     plt.errorbar(n_range,mean_error,yerr=std_error,linewidth=3)
     plt.xlabel('n')
     plt.ylabel('Mean square error')
-    #plt.show()
+    plt.show()
 
-def kfold_GradientBoostingRegressor(X, y, k, poly):
+def kfold_GradientBoostingRegressor(X, y, poly):
     kf = KFold(n_splits=5)
     mean_error=[]; std_error=[]
     Xpoly = PolynomialFeatures(poly).fit_transform(X)
     c_range = [0.0001, 0.005, 0.01, 0.05, 0.1, 0.5]
     for c in c_range:
         model = GradientBoostingRegressor(alpha=c)
-        scores = kfold_calculation(Xpoly, y, k, model)
+        scores = kfold_calculation(Xpoly, y, model)
         mean_error.append(np.array(scores).mean())
         std_error.append(np.array(scores).std())
     plt.title('Gradient Boosting regression alpha choice')
     plt.errorbar(c_range,mean_error,yerr=std_error,linewidth=3)
     plt.xlabel('c')
     plt.ylabel('Mean square error')
-    #plt.show()
+    plt.show()
 
 # Going to use this to find the best 
 def findBestK(coords, features, targets):
@@ -169,47 +203,46 @@ def clusterModel(features, targets):
     features.to_csv('data/clusteredData.csv', index=None)
     return features
 
-df = pd.read_csv("data/houseListings.csv")
-df = df.drop(['Address', 'Construction', 'Parking'], axis=1)
-data = normalize(df)
-
-targets = data['Price']
-features = data.drop(['Price'], axis=1)
-features = clusterModel(features, targets)
+df = pd.read_csv("../data/houseListings.csv")
+tmp = pd.read_csv("../data/clusteredData.csv")
+target = df['Price']
+features = df.drop(['Price'], axis=1)
+features = tmp
 #One-hot encoding for cluster numbers
-features = pd.concat([features, pd.get_dummies(features['cluster_label'], prefix='cluster')], axis=1)
-features = features.drop(['cluster_label'], axis=1)
+# features = pd.concat([features, pd.get_dummies(features['cluster_label'], prefix='cluster')], axis=1)
+# features = features.drop(['cluster_label'], axis=1)
 
-# data = pd.read_csv("data/houseListings.csv")
-# # According to the small data set, testing with removing those cols with most 0
-# data = data.drop(['Address', 'Construction', 'Parking'],axis=1)
-# # Sort data
-# data.sort_values('Price',ascending=False)
-# # Normalize data
-# data_norm = normalize(data)
+data = pd.read_csv("../data/houseListings.csv")
+# According to the small data set, testing with removing those cols with most 0
+data = data.drop(['Address', 'Construction', 'Parking'],axis=1)
+# Sort data
+data.sort_values('Price',ascending=False)
+# Normalize data
+data_norm = normalize(data)
 
-# labels = data_norm['Price']
-# train = data_norm.drop(['Price'],axis=1)
-# train = features.drop(['Address', 'Construction', 'Parking'],axis=1)
+labels = data_norm['Price']
+train = data_norm.drop(['Price'],axis=1)
+train = features.drop(['Address', 'Construction', 'Parking'],axis=1)
 
-#print(features)
+print(train)
+model_polynomials(train, labels)
 #kfold_polynomials(train, labels, 5)
 
-kfold_Lasso(features, targets, 5, 1)
+# kfold_Lasso(train, labels, 5, 1)
 
-kfold_Ridge(features, targets, 5, 1)
+# kfold_Ridge(train, labels, 5, 1)
 
-kfold_XGBRegressor(features, targets, 5, 1)
+# kfold_XGBRegressor(train, labels, 5, 1)
 
-kfold_RandomForestRegressor(features, targets, 5, 1)
+# kfold_RandomForestRegressor(train, labels, 5, 1)
 
-kfold_GradientBoostingRegressor(features, targets, 5, 1)
+# kfold_GradientBoostingRegressor(train, labels, 5, 1)
 
-dummy = DummyRegressor(strategy='mean').fit(features, targets)
-ydummy = dummy.predict(features)
-print('Dummy RMSE:' + str(np.sqrt(mean_squared_error(targets, ydummy))))
+dummy = DummyRegressor(strategy='mean').fit(train, labels)
+ydummy = dummy.predict(train)
+print('Dummy RMSE:' + str(np.sqrt(mean_squared_error(labels, ydummy))))
 
-x_train , x_test , y_train , y_test = train_test_split(features , targets , test_size = 0.2 ,random_state =2)
+x_train , x_test , y_train , y_test = train_test_split(train , labels , test_size = 0.2 , random_state =42)
 
 # Create a XGBoost Regressor
 print('\nRunning XGBoost Regression:')
